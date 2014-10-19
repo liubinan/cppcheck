efbbcd9599098e4c7c93f4981a18d7121982ddcd
liubinan <liubinan@liubinan.net>
2014-10-20 00:25:43 +0800

    use premake
    use premake

---
+++ makefiles/gen_makefiles.bat
@@ -0,0 +1,8 @@
+premake4.exe --os=windows vs2010
+premake4.exe --os=windows gmake
+premake4.exe --os=windows codelite
+premake4.exe --os=windows codeblocks
+
+premake4.exe --os=linux gmake
+premake4.exe --os=linux codelite
+premake4.exe --os=linux codeblocks
---
+++ makefiles/premake4.lua
@@ -0,0 +1,130 @@
+
+function getPlatDir(plat)
+    if os.get() == "windows" then
+        if plat == "x32" then
+            return "win32"
+        else
+            return"win64"
+        end
+    elseif os.get() == "linux" then
+        if plat == "x32" then
+            return "linux32"
+        else
+            return "linux64"
+        end
+    elseif os.get() == "solaris" then
+        return "solaris"
+    end
+    return "unkown"
+end
+
+function setTargetObjDir(outDir)
+	for _, cfg in ipairs(configurations()) do
+		for _, plat in ipairs(platforms()) do
+			targetPath = outDir .. "/" .. string.lower(cfg) .. "/" .. getPlatDir(plat)
+			
+			configuration {cfg, plat}
+				targetdir(targetPath)
+		end
+	end
+end
+
+function setLibDir(libDir)
+    if (libDir == "" or libDir == nil) then
+        libDir = "."
+    end
+    for _, plat in ipairs(platforms()) do
+        configuration {plat}
+            libdirs {
+                libDir .. "/" .. getPlatDir(plat)
+            }
+    end
+end
+
+solution "cppcheck"
+    -- 解决方案全局设置
+    configurations {"Debug", "Release"}
+    platforms {"x32", "x64"}
+    location (os.get() .. "/" .. iif(_ACTION, _ACTION, "unkown"))
+    flags {"StaticRuntime"}
+    
+    configuration "debug"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+
+	configuration "release"
+		defines { "NDEBUG" }
+		flags { "Optimize" }
+    
+    configuration "vs*"
+        defines {"WIN32", "_CRT_SECURE_NO_WARNINGS"}
+        buildoptions {"-wd4996"}
+ 
+    -- 设置cli工程
+    project "cli"
+		targetname "cppcheck"
+        kind "ConsoleApp"
+        language "C++"
+        files { 
+			"../cli/**.h", 
+			"../cli/*.cpp",
+		}
+        includedirs { 
+			"../cli",
+			"../lib",
+		}
+        links {"cppcheck"}
+        targetdir("bin")
+		
+		configuration {"gmake or codeblocks or codelite"}
+			buildoptions {"-std=gnu++11"}
+		
+		configuration {"gmake or codeblocks or codelite", "windows"}
+			links {"shlwapi"}
+
+    project "cppcheck"
+        kind "SharedLib"
+        language "C++"
+        files {
+            "../lib/*.h", 
+			"../lib/*.cpp",
+			"../externals/tinyxml/*.h",
+			"../externals/tinyxml/*.cpp",
+			"../externals/fflua/**.h",
+        }
+		--excludes {"../lib/lua*.cpp"}
+        includedirs {
+            "../lib",
+			"../lua",
+			"../externals/tinyxml",
+			"../externals/fflua",
+        }
+		links {"lua"}
+        targetdir("bin")
+		
+		configuration {"gmake or codeblocks or codelite"}
+			buildoptions {"-std=gnu++11"}
+
+	project "lua"
+        kind "StaticLib"
+        language "C"
+        files {
+            "../lua/*.h", 
+			"../lua/*.c"
+        }
+        includedirs {
+            "../lua",
+        }
+        targetdir("bin")
+		
+		configuration "vs*"
+			includedirs { "../third_party/gflags/src/windows" }
+			defines {
+				"LUA_WIN",
+			}
+            pchheader "common.h"
+            pchsource "../src/common.cpp"
+            links {"libeay32", "ws2_32"}
+
+
+            
